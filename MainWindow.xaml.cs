e8bd9fb90f6724a8659ec668e48f0d69ee0f741b
Siyabonga Ncube <104077539+BlackSIYA11@users.noreply.github.com>
2024-04-10 22:22:51 +0200

    Add files via upload

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,19 @@
+﻿using System.Windows;
+
+namespace Inventory_System_WPF
+{
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Login_Click(object sender, RoutedEventArgs e)
+        {
+            // Replace the ContentGrid with the Login page content
+            ContentGrid.Children.Clear();
+            //ContentGrid.Children.Add(new Login());
+        }
+    }
+}
