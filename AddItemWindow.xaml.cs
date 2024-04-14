c23af2f03a9f005e932013987ce6e4db26683480
Dominique <127980415+dominiqueprogram@users.noreply.github.com>
2024-04-14 18:00:23 +0200

    Add files via upload

---
+++ AddItemWindow.xaml.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Windows;
+using InventoryManagementLibrary; 
+
+namespace InventoryManagement
+{
+    public partial class AddItemWindow : Window
+    {
+        private void SaveButton_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+        //Logic of the window
+
+
+    }
+}
